c0544e0975a2365cd965dc988c318a2bd4cbe459
Mahendra Kumar <mahendrakumar@mahendras-mbp.home>
2018-05-19 17:02:54 +0200

    route params

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Switch, NavLink, Link} from 'react-router-dom';
 const Home = () => (<div> Home Component </div>);
 const About = () => (<div> About Component </div>);
 const Contact = () => (<div> Contact Component </div>);
+const User = ({match}) => (<div> Welcome {match.params.userid}</div>);
 const Error = () => (<div> Error: Page not found !!!! </div>);
 
 class App extends Component {
@@ -16,11 +17,13 @@ class App extends Component {
             <li> <NavLink to="/"> Home </NavLink> </li>
             <li> <NavLink to="/about"> About </NavLink> </li>
             <li> <NavLink to="/contact"> Contact </NavLink> </li>
+            <li> <NavLink to="/user/mahendra"> User </NavLink> </li>
           </ul>
           <Switch>
             <Route path='/' component={Home} exact={true}/>
             <Route path='/about' component={About}/>
             <Route path='/contact' component={Contact}/>
+            <Route path='/user/:userid' exact strict component={User}/>
             <Route component={Error}/>
          </Switch>
         </div>
