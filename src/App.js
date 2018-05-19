5676f9993aaa638a989f92e8e2c394d53a5c0179
Mahendra Kumar <mahendrakumar@mahendras-mbp.home>
2018-05-19 12:10:48 +0200

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Switch, NavLink, Link} from 'react-router-dom';
 
-const NewComp = () => {
-  return (
-
-    <div>
-      New Route
-    </div>
-  )
-}
+const Home = () => (<div> Home Component </div>);
+const About = () => (<div> About Component </div>);
+const Contact = () => (<div> Contact Component </div>);
 
 class App extends Component {
   render() {
     return (
-      
-      <BrowserRouter> 
-        <Route path='/new' component={NewComp}/>
-      </BrowserRouter>
+
+        <BrowserRouter>
+        <div>
+          <ul>
+            <li> <NavLink to="/"> Home </NavLink> </li>
+            <li> <NavLink to="/about"> About </NavLink> </li>
+            <li> <NavLink to="/contact"> Contact </NavLink> </li>
+          </ul>
+          <Switch>
+            <Route path='/' component={Home} exact={true}/>
+            <Route path='/about' component={About}/>
+            <Route path='/contact' component={Contact}/>
+         </Switch>
+        </div>
+    
+ 
+        </BrowserRouter>
+
     )
   }
 }
